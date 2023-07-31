5fb3f6f8e8d8e4228f9ec1803206465e514ef609
Jai <138092469+jaithecore@users.noreply.github.com>
2023-07-31 11:40:16 +0530

    update the basePath

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  basePath: "",
+  basePath: "/test-for-profile",
   images: {
     unoptimized: true,
   },
